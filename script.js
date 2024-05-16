e51b2be5b3c5ec0a21102f8393ff599e8ee33720
Matheus Miranda <mme@cesar.school>
2024-05-16 15:19:44 -0300

    ajustes do container

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ function fecharMenu(){
     document.getElementById("menu").style.width = "0px";
     document.getElementById("conteudo").style.marginLeft = "0px";
 }
+function conversorBin(){
+var binario = document.getElementById("bin-input");
+var decimal = parseInt(binario, 2);
+console.log(decimal);
+}
+function conversorHexa(){
+var hexadecimal = document.getElementById("hexa-input");
+var decimal = parseInt(hexadecimal, 16);
+console.log(decimal);
+}
