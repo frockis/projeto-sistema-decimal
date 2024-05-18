bc867a02e09759604b675759351f8a77f7155f17
Matheus Miranda <mme@cesar.school>
2024-05-18 14:07:47 -0300

    Ajustes no design e adicionado o conversor e butao para o quiz

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 function abrirMenu(){
     document.getElementById("menu").style.width = "250px";
+    document.getElementById("menu").style.display = "flex";
     document.getElementById("conteudo").style.marginLeft = "250px";
 }
 
 function fecharMenu(){
     document.getElementById("menu").style.width = "0px";
-    document.getElementById("conteudo").style.marginLeft = "0px";
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("conteudo").style.marginLeft = "";
 }
 
-//Initial References
-let decInput = document.getElementById("dec-input");
-let binInput = document.getElementById("bin-input");
-let hexaInput = document.getElementById("hexa-input")
-let errorMsg = document.getElementById("error-msg");
-
-//Convert decimal to binary when user inputs in the decimal field
-decInput.addEventListener("input", () => {
-  let decValue = parseInt(decInput.value);
-  //Converts the decimal value to binary
-  binInput.value = decValue.toString(2);
-});
-
-//Convert binary to decimal when user inputs in the binary field
-binInput.addEventListener("input", () => {
-  let binValue = binInput.value;
-  //If the binary number is valid convert it to decimal
-  if (binValidator(binValue)) {
-    decInput.value = parseInt(binValue, 2);
-    errorMsg.textContent = "";
-  }
-  //Else display an error message
-  else {
-    errorMsg.textContent = "Please Enter An Valid Binary Input";
-  }
-
-  //Function to check if the binary number is valid i.e it does not contain any number other than 0 and 1
-  function binValidator(num) {
-    for (let i = 0; i < num.length; i++) {
-      if (num[i] != "0" && num[i] != "1") {
-        return false;
-      }
+function convert(id) {
+    let decimalInput = document.getElementById("decimal");
+    let hexadecimalInput = document.getElementById("hexadecimal");
+    let binaryInput = document.getElementById("binary");
+  
+    if (id === "decimal") {
+      let decimal = parseInt(decimalInput.value);
+      if (!isNaN(decimal)){
+        hexadecimalInput.value = decimal.toString(16);
+        binaryInput.value = decimal.toString(2);
+        }else{
+            hexadecimalInput.value = "";
+            binaryInput.value = "";
+        }
+      } else if (id === "hexadecimal") {
+        let decimal = parseInt(hexadecimalInput.value, 16);
+        if (!isNaN(decimal)){
+        decimalInput.value = decimal;
+        binaryInput.value = decimal.toString(2);
+    } else{
+        decimalInput.value = "";
+        binaryInput.value = "";
     }
-    return true;
-  }
-});
+      } else if (id === "binary") {
+        let decimal = parseInt(binaryInput.value, 2);
+        if (!isNaN(decimal)){
+        decimalInput.value = decimal;
+        hexadecimalInput.value = decimal.toString(16);
+        } else{
+            decimalInput.value = "";
+            hexadecimalInput.value = "";
+        }
+        }
+    }
