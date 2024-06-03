f39e2bcf77d55ac2958ae04afd1b2cbb4fa69a8a
Matheus Miranda <mme@cesar.school>
2024-06-02 23:50:56 -0300

    style e imagens

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function abrirMenu(){
     document.getElementById("menu").style.width = "250px";
     document.getElementById("menu").style.display = "flex";
-    document.getElementById("conteudo").style.marginLeft = "250px";
+    document.getElementById("conteudo").style.marginLeft = "";
 }
 
 function fecharMenu(){
